9bfd5a77bcac50672d60bac2e0ac6afe2dc06188
tmac <727924283@qq.com>
2021-07-28 17:56:07 +0800

    底部tabbar跟换

--- minecenter/lib/root_page.dart
+++ minecenter/lib/root_page.dart
@@ -9,61 +9,102 @@ class MainPage extends StatefulWidget {
   _MainPageState createState() => _MainPageState();
 }
 
+const Map<String, String> _bottomNames = {
+  'all': '主页',
+  'favorites': '音乐',
+  'play': '',
+  'pic': '小视频',
+  'integral': '我的'
+};
+
 class _MainPageState extends State<MainPage> {
-  int _pageIndex = 0;
-  List<Widget> pages = <Widget>[
+  //当前选中索引
+  int _currentIndex = 0;
+
+  //页面集合
+  final List<Widget> _pages = <Widget>[
     EgHomePage(),
     HomePage(),
     Container(),
     HomePage(),
     MinePage(),
   ];
+  //底部item数组
+  final List<BottomNavigationBarItem> _bottomNavBarList = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _bottomNames.forEach((key, value) {
+      _bottomNavBarList.add(_bottomNavigationBarItem(key, value));
+    });
+  }
+
+  //底部切换
+  void _onTabClick(int value) {
+    if (value == 2) {
+      return _onCreateMedia();
+    }
+    setState(() {
+      _currentIndex = value;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text('主页面'),
-      // ),
+      body: _pages[_currentIndex],
       drawer: mydrawer(),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                size: 20,
-              ),
-              title: Text(
-                '主页',
-                style: TextStyle(fontSize: 14),
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.favorite,
-                size: 20,
-              ),
-              title: Text('喜欢')),
-          BottomNavigationBarItem(
-              icon: Icon(
-                Icons.music_video,
-                size: 20,
-              ),
-              title: Text('我的')),
-        ],
-        onTap: (value) {
-          setState(() {
-            _pageIndex = value;
-          });
-          return Text(_pageIndex.toString());
-        },
-        currentIndex: _pageIndex,
+        items: _bottomNavBarList,
+        currentIndex: _currentIndex,
+        onTap: _onTabClick,
+        type: BottomNavigationBarType.fixed,
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.grey,
       ),
-      body: Center(
-        child: pages[_pageIndex],
+      floatingActionButton: _creatMediaButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  //发布按钮
+  Widget _creatMediaButton() {
+    return Container(
+      width: 44,
+      height: 44,
+      margin: EdgeInsets.only(top: 56),
+      child: FloatingActionButton(
+        child: Image.asset(
+          'assets/images/icons/play.png',
+        ),
+        onPressed: _onCreateMedia,
       ),
     );
+  }
+
+  //每一项baritem
+  BottomNavigationBarItem _bottomNavigationBarItem(String key, String value) {
+    return BottomNavigationBarItem(
+      icon: Image.asset(
+        'assets/images/icons/$key.png',
+        width: 24,
+        height: 24,
+      ),
+      activeIcon: Image.asset(
+        'assets/images/icons/$key-fill.png',
+        width: 24,
+        height: 24,
+      ),
+      label: value,
+      tooltip: '',
+    );
+  }
+
+  void _onCreateMedia() {
+    print('_onCreateMedia');
   }
 }
 
@@ -121,3 +162,49 @@ class _mydrawerState extends State<mydrawer> {
     );
   }
 }
+
+/**
+ * Scaffold(
+      // appBar: AppBar(
+      //   title: Text('主页面'),
+      // ),
+      drawer: mydrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 20,
+              ),
+              title: Text(
+                '主页',
+                style: TextStyle(fontSize: 14),
+              )),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.favorite,
+                size: 20,
+              ),
+              title: Text('喜欢')),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.music_video,
+                size: 20,
+              ),
+              title: Text('我的')),
+        ],
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+          return Text(_currentIndex.toString());
+        },
+        currentIndex: _currentIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+      ),
+      body: Center(
+        child: pages[_currentIndex],
+      ),
+    );
+ */
